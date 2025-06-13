67f9f71781d87f70ac545fd7090d39f40095b0ee
BeatrizMariaPessoa <pessoa.beatriz@escolar.ifrn.edu.br>
2025-06-13 18:59:07 -0300

    adicionado a home um link de tarefas

--- todo-app/src/app/page.tsx
+++ todo-app/src/app/page.tsx
@@ -1,60 +1,12 @@
 "use client";
 import type React from "react";
+import Link from "next/link";
 
 import { useEffect, useState } from "react";
 import dados, { TarefaInterface } from "@/data";
-import Cabecalho from "@/componentes/Cabecalho";
-import ModalTarefa from "@/componentes/ModalTarefa";
 
 
-interface TarefaProps {
-  titulo: string;
-  concluido?: boolean;
-}
 
-const Tarefa: React.FC<TarefaProps> = ({ titulo, concluido }) => {
-  const [estaConcluido, setEstaConcluido] = useState(concluido);
-
-  const classeCard = `p-3 mb-3 rounded-lg shadow-md hover:cursor-pointer hover:border ${
-    estaConcluido
-      ? "bg-gray-800 hover:border-gray-800"
-      : "bg-gray-400 hover:border-gray-400"
-  }`;
-
-  const classeCorDoTexto = estaConcluido ? "text-amber-50" : "";
-
-  const escutarClique = () => {
-    console.log(`A tarefa '${titulo}' foi clicada!`);
-    setEstaConcluido(!estaConcluido);
-  };
-
-  return (
-    <div className={classeCard} onClick={() => escutarClique()}>
-      <h3 className={`text-xl font-bold ${classeCorDoTexto}`}>{titulo}</h3>
-      <p className={`text-sm ${classeCorDoTexto}`}>
-        {estaConcluido ? "Concluída" : "Pendente"}
-      </p>
-    </div>
-  );
-};
-
-interface TareafasProps {
-  dados: TarefaInterface[];
-}
-
-const Tarefas: React.FC<TareafasProps> = ({ dados }) => {
-  return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-      {dados.map((tarefa) => (
-        <Tarefa
-          key={tarefa.id}
-          titulo={tarefa.title}
-          concluido={tarefa.completed}
-        />
-      ))}
-    </div>
-  );
-};
 
 const Home = () => {
   const [mostrarModal, setMostrarModal] = useState(false);
@@ -62,10 +14,11 @@ const Home = () => {
 
   return (
     <div className="container mx-auto p-4">
-      <Cabecalho />
-      <button className="mb-1 bg-gray-600 text-white p-2 font-semibold rounded-lg" onClick={() => setMostrarModal(true)}>Adicionar Tarefa +</button>
-      {mostrarModal && <ModalTarefa tarefas={tarefas} setTarefas={setTarefas} onClose={() => setMostrarModal(false)} />}
-      <Tarefas dados={tarefas} />
+      <Link href="/tarefas">
+        <button className="mb-1 bg-gray-600 cursor-pointer text-white p-2 font-semibold rounded-lg">
+          Tarefas
+        </button>
+      </Link>
     </div>
   );
 };
